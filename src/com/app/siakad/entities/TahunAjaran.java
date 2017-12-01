1fac4fc5ae140921098a46d96d7eb75ae591e90c
khairulumar <32088334+khairulumar@users.noreply.github.com>
2017-12-02 02:25:11 +0700

    Add files via upload

---
+++ src/com/app/siakad/entities/TahunAjaran.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.siakad.entities;
+
+import com.app.siakad.database.KoneksiDB;
+
+/**
+ *
+ * @author Ritky
+ */
+public class TahunAjaran extends KoneksiDB {
+    
+}
